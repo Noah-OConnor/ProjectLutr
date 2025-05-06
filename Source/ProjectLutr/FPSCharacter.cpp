d9793ae62dc5a264b9f38484e9bcc5ae380bb82c
Noah <no998698@ucf.edu>
2025-05-06 00:40:08 -0400

    Revert "Started Setting up Backend based on Tranek Documentation"
    This reverts commit 07fa9314cd8a01c7426d427af9a52aca9c72ce76.

--- Source/ProjectLutr/FPSCharacter.cpp
+++ Source/ProjectLutr/FPSCharacter.cpp
@@ -2,81 +2,33 @@
 
 
 #include "FPSCharacter.h"
-#include "PlayerAttributeSet.h"
-#include "FPSCharacterMovementComponent.h"
-#include "FPSAbilitySystemComponent.h"
-#include "Components/CapsuleComponent.h"
 
 // Sets default values
-AFPSCharacter::AFPSCharacter(const class FObjectInitializer& ObjectInitializer) :
-	Super(ObjectInitializer.SetDefaultSubobjectClass<UFPSCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+AFPSCharacter::AFPSCharacter()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Overlap);
-
-	bAlwaysRelevant = true;
-}
-
-UAbilitySystemComponent * AFPSCharacter::GetAbilitySystemComponent() const
-{
-	return AbilitySystemComponent.Get();
-}
-
-bool AFPSCharacter::IsAlive() const
-{
-	return GetHealth() > 0.0f;;
-}
-
-//void AFPSCharacter::RemoveCharacterAbilities()
-//{
-//}
-
-float AFPSCharacter::GetHealth() const
-{
-	if (AttributeSet.IsValid())
-	{
-		return AttributeSet->GetHealth();
-	}
-
-	return 0.0f;
-}
-
-float AFPSCharacter::GetMaxHealth() const
-{
-	if (AttributeSet.IsValid())
-	{
-		return AttributeSet->GetMaxHealth();
-	}
-
-	return 0.0f;
 }
 
 // Called when the game starts or when spawned
 void AFPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
-//void AFPSCharacter::AddCharacterAbilities()
-//{
-//
-//}
-//
-//void AFPSCharacter::InitializeAttributes()
-//{
-//}
-//
-//void AFPSCharacter::AddStartupEffects()
-//{
-//}
-
-void AFPSCharacter::SetHealth(float Health)
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
 {
-	if (AttributeSet.IsValid())
-	{
-		AttributeSet->SetHealth(Health);
-	}
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
 }
 
