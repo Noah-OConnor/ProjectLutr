b732e398aa7316d6140d6358bd7ff8c66e64197d
Noah <no998698@ucf.edu>
2025-05-05 22:28:17 -0400

    Imported GD and Replaced some names

--- Source/ProjectLutr/ProjectLutr.Build.cs
+++ Source/ProjectLutr/ProjectLutr.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright 2020 Dan Kestranek.
 
 using UnrealBuildTool;
 
@@ -7,17 +7,15 @@ public class ProjectLutr : ModuleRules
 	public ProjectLutr(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            "Slate",
+            "SlateCore",
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks"
+        });
+    }
 }
